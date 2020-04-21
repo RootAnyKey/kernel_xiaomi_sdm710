1e0275818a578a46155111588a00d59610e1d721
XGMO9 <941558072@QQ.COM>
2020-04-21 16:11:47 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+export ARCH=arm64
+export SUBARCH=arm64
+CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=/home/mo9/tc/gcc644.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/mo9/tc/gcc4.9/bin/arm-linux-androideabi-
+make O=out sirius_defconfig
+make -j$(nproc --all) O=out \
+ARCH=arm64 \
+SUBARCH=arm64 \
+CC=/home/mo9/tc/aosp-clang/bin/clang \
+CLANG_TRIPLE=aarch64-linux-gnu-
